1e50cf4aec6f7e23f405a2c70475eb001c060bd5
Alvin-progg <doctoralvin726@gmail.com>
2024-08-04 23:18:00 +0800

    fix size of text in carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -17,9 +17,9 @@ const Carousel = ({ data }) => {
       {data.map((project) => (
         <div key={project.id} className="flex justify-center">
           <div className="w-3/4 m-auto border-white border rounded-xl  p-4 gap-4 ">
-            <div className="flex-grow">
-              <span className="text-3xl lg:text-4xl text-white">{project.title}</span>
-              <p className="text-gray-500 text-2xl lg:3xl">{project.disc}</p>
+            <div className="flex-col gap-10 p-3 ">
+              <span className="text-2xl lg:text-4xl text-white mb-6">{project.title}</span>
+              <p className="text-gray-500 text-xl lg:3xl">{project.disc}</p>
             </div>
             <div className="gap-6 flex text-white mt-5">
               <a href={project.demo} className="border hover:bg-white hover:text-black border-white rounded-lg m-2 p-2">Demo</a>
